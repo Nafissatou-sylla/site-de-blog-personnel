ed826f618dd6bca2f0a6d64224de89c62b9d56a1
Sylla <finasylla2020@gmail.com>
2023-03-23 14:47:58 +0100

    entité MotCle

---
+++ siteDeBlog/src/Entity/MotsCles.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MotsClesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MotsClesRepository::class)]
+class MotsCles
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 15, nullable: true)]
+    private ?string $mot = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMot(): ?string
+    {
+        return $this->mot;
+    }
+
+    public function setMot(?string $mot): self
+    {
+        $this->mot = $mot;
+
+        return $this;
+    }
+}
